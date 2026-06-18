6f00e6eb5108f1cffc36c193961189be13424658
amaananwar46 <amaananwar46@gmail.com>
2026-06-18 20:02:24 +0530

    add url in cors

--- server/server.ts
+++ server/server.ts
@@ -20,7 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin:['http://localhost:5173','http://localhost:3000'],
+    origin:['http://localhost:5173','http://localhost:3000','https://thumbly-client.vercel.app'],
     credentials:true
 }))
 
